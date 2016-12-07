50bfd19a71c1b840755b3f00516d34c73dbeaf3a
dipanjan <dipanzan.sarkar@gmail.com>
2016-12-08 01:50:34 +0530

    Added code for chapter 5

---
+++ Chapter-5/utils.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 03 12:38:45 2016
+
+@author: DIP
+"""
+
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+
+def build_feature_matrix(documents, feature_type='frequency'):
+
+    feature_type = feature_type.lower().strip()  
+    
+    if feature_type == 'binary':
+        vectorizer = CountVectorizer(binary=True, min_df=1, 
+                                     ngram_range=(1, 1))
+    elif feature_type == 'frequency':
+        vectorizer = CountVectorizer(binary=False, min_df=1, 
+                                     ngram_range=(1, 1))
+    elif feature_type == 'tfidf':
+        vectorizer = TfidfVectorizer(min_df=1, 
+                                     ngram_range=(1, 1))
+    else:
+        raise Exception("Wrong feature type entered. Possible values: 'binary', 'frequency', 'tfidf'")
+
+    feature_matrix = vectorizer.fit_transform(documents).astype(float)
+    
+    return vectorizer, feature_matrix
+
+
+from scipy.sparse.linalg import svds
+    
+def low_rank_svd(matrix, singular_count=2):
+    
+    u, s, vt = svds(matrix, k=singular_count)
+    return u, s, vt
+    
